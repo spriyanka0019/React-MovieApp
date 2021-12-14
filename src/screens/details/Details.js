3fc784bd556117fc30b8c480a19dd0cb542d80e0
spriyanka0019 <spriyanka0019@gmail.com>
2021-12-14 09:20:18 +0530

    Added Dummy Values in left, Right & Middle flex container of Details.js

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -5,8 +5,52 @@ import { Link } from 'react-router-dom';
 import './Details.css';
 
 class Details extends Component {
+    constructor() {
+        super();
+        this.state = {
+            movie: {
+                genres: [],
+                trailer_url: "",
+                artists: []
+            },
+            starIcons: [
+                {
+                    id: 1,
+                    stateId: "star1",
+                    color: "black"
+                },
+                {
+                    id: 2,
+                    stateId: "star2",
+                    color: "black"
+                },
+                {
+                    id: 3,
+                    stateId: "star3",
+                    color: "black"
+                },
+                {
+                    id: 4,
+                    stateId: "star4",
+                    color: "black"
+                },
+                {
+                    id: 5,
+                    stateId: "star5",
+                    color: "black"
+                }]
+        }
+    }
 
     render() {
+        let movie = this.state.movie;
+        const opts = {
+            height: '300',
+            width: '700',
+            playerVars: {
+                autoplay: 1
+            }
+        }
 
         return (
             <div className="details">
@@ -17,16 +61,26 @@ class Details extends Component {
                     </Typography>
                 </div>
                 <div className="flex-container">
-                    <div className="left">
-
+                    <div className="left-detail">
+                        <img src={movie.poster_url} alt={movie.title} />
                     </div>
 
-                    <div className="middle">
-
+                    <div className="middle-detail">
+                        <div>
+                            <Typography variant="headline" component="h2">{movie.title} </Typography>
+                        </div>
+                        <br />
+                        <div>
+                            <Typography>
+                                <span className="bold">Genres: </span> {movie.genres.join(', ')}
+                            </Typography>
+                        </div>
                     </div>
 
-                    <div className="right">
-
+                    <div className="right-detail">
+                        <Typography>
+                            <span className="bold">Rate this movie: </span>
+                        </Typography>
                     </div>
 
                 </div>
