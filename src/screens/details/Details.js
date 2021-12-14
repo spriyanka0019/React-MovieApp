16f58715c33f189dd2700914d7039332ec50ae54
spriyanka0019 <spriyanka0019@gmail.com>
2021-12-14 09:29:42 +0530

    Added XmlRequest to Fetch the movie data

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -42,6 +42,23 @@ class Details extends Component {
         }
     }
 
+    componentWillMount() {
+        let that = this;
+        let dataMovie = null;
+        let xhrMovie = new XMLHttpRequest();
+        xhrMovie.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                that.setState({
+                    movie: JSON.parse(this.responseText)
+                });
+            }
+        });
+
+        xhrMovie.open("GET", this.props.baseUrl + "movies/" + this.props.match.params.id);
+        xhrMovie.setRequestHeader("Cache-Control", "no-cache");
+        xhrMovie.send(dataMovie);
+    }
+
     render() {
         let movie = this.state.movie;
         const opts = {
