2852e95c20da1c8668ad95369514e9e4aa8a660d
spriyanka0019 <spriyanka0019@gmail.com>
2021-12-14 08:58:44 +0530

    Added Back Button in Details.js

---
+++ src/screens/details/Details.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Header from '../../common/header/Header';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import './Details.css';
+
+class Details extends Component {
+
+    render() {
+       
+        return (
+            <div className="details">
+                <Header baseUrl={this.props.baseUrl} showBookShowButton="true" />
+                <div className="button-back">
+                    <Typography>
+                        <Link to="/">  &#60; Back to Home</Link>
+                    </Typography>
+                </div>
+               
+            </div>
+        )
+    }
+}
+
+export default Details;
